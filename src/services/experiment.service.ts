2332e5829aaa7f6ab782b70d9cc0c14b3b9a68d9
Wei Yihao <weiyihao@amazon.com>
2026-04-14 02:45:05 +0800

    fix: use valid createdByType for results log document

--- src/services/experiment.service.ts
+++ src/services/experiment.service.ts
@@ -991,7 +991,7 @@ async function appendExperimentResultsLog(
         type: DOCUMENT_TYPE,
         content: CSV_HEADER + "\n" + newRow,
         createdByUuid: "system",
-        createdByType: "system",
+        createdByType: "user",
       },
     });
   }
